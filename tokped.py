c085b5c7239d385739746482040de54871f4959e
Alvin201 <44694587+Alvin201@users.noreply.github.com>
2021-07-30 23:08:47 +0700

    Update tokped.py

--- tokped.py
+++ tokped.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 
 
 n = input('please enter the term : ')
+p = int(input('please enter the page : '))
 #url = 'https://www.tokopedia.com/search?st=product&q={}'.format(n)
-url = 'https://www.tokopedia.com/search?navsource=home&page=&q={}'.format(n)
+# url = 'https://www.tokopedia.com/search?navsource=home&page={}&q={}'.format(p,n)
 headers = {
         'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Encoding' : 'gzip, deflate, br',
@@ -16,51 +17,59 @@ headers = {
         }
 datas = []
 count_page = 0
-for page in range(1, 11):
-        count_page+=1
-        print('scraping page :',count_page)
+for i in range(p):
+        i += 1
+        url = 'https://www.tokopedia.com/search?navsource=home&page={}&q={}&st=product'.format(i, n)
+        print(url)
         #r = requests.get(url_token, headers=headers).json()
-        r = requests.get(url+str(page), headers=headers)
-        #print(r.status_code)
+        r = requests.get(url, headers=headers)
+        # print(r.url)
         soup = BeautifulSoup(r.text, 'html.parser')
         #print(soup)
-        items = soup.find_all('div', 'css-12sieg3')
+        items = soup.find_all('div', {'class': 'css-12sieg3'})
         for it in items:
-                try : nm = ''.join(it.find('div', {'data-testid':'spnSRPProdName'}).text.strip().split('\n'))
-                except : nm = ''
-                try : price = ''.join(it.find('div', {'data-testid':'spnSRPProdPrice'}).text.strip().split('\n'))
-                except : price = ''
-                try :
-                        shloc = ''.join(it.find('div', 'css-1pznt2j').text.strip().split('\n'))
-                except : shloc = ''
-                try:
-                        rating = ''.join(it.find('span','css-etd83i').text.strip().split('\n'))
-                except:
-                        rating = ''
-                try:
-                        sold = ''.join(it.find('span', 'css-1kgbcz0').text.strip().split('\n')).replace('Terjual','')
-                except:
-                        sold = ''
+            try :
+                nm = ''.join(it.find('div', {'data-testid':'spnSRPProdName'}).text.strip().split('\n'))
+            except : nm = ''
 
+            try :
+                price = ''.join(it.find('div', {'data-testid':'spnSRPProdPrice'}).text.strip().split('\n'))
+            except : price = ''
 
-                print(nm,price,shloc,sold)
-                datas.append([
-                        nm,
-                        price,
-                        shloc,
-                        rating,
-                        sold
-                ])
+            #shop locator and shop name in 1 div
+            data = [d.find_all('span') for d in it.find_all('div', {'class': 'css-1rn0irl'})]
+            temp = []
+            value = ""
+            test = ""
+            for i in data:
+                for j in i:
+                    temp.append(j.text)
+                    a = ' '.join(str(x) for x in temp)
+                    print(a)
+
+            try:
+                rating = ''.join(it.find('span','css-etd83i').text.strip().split('\n'))
+            except:
+                rating = ''
+
+            try:
+                sold = ''.join(it.find('span', 'css-1kgbcz0').text.strip().split('\n')).replace('Terjual','')
+            except:
+                sold = ''
+
+            print(nm.replace("!",''),price,a,rating,sold)
+            datas.append([nm.replace("!",''),price,a,rating,sold])
 
 head = ['Name', 'Price','Shop Loc','Rating','Sold']
 writer = csv.writer(open('results/{}.csv'.format(n), 'w', newline=''))
 writer.writerow(head)
-
-
-for d in datas:
+with open('results/{}.csv'.format(n), 'w', newline='') as out:
+    writer = csv.writer(out, delimiter='|')
+    writer.writerow(head)
+    for d in datas:
         if any(field.strip() for field in d):  # remove blank rows
-                try:
-                        writer.writerow(d)
-                except: ''
-print('Export Successfully')
+            try:
+                writer.writerow(d)
+            except: ''
+    print('Export Successfully')
 
